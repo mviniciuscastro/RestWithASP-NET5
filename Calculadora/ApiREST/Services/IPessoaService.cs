4d36dc60f45479eccad3ae07e9c8c71530de0b50
Marcus Vinícius <mviniciuscastro@gmail.com>
2020-10-21 19:38:41 -0300

    interface

---
+++ Calculadora/ApiREST/Services/IPessoaService.cs
@@ -0,0 +1,19 @@
+﻿using ApiREST.Model;
+using System.Collections.Generic;
+
+namespace ApiREST.Services
+{
+    public interface IPessoaService
+    {
+        Pessoa Create(Pessoa pessoa);
+
+        Pessoa FindByID(long id);
+
+        Pessoa Update(Pessoa pessoa);
+
+        void Delete(long id);
+
+        List<Pessoa> FindAll();
+
+    }
+}
