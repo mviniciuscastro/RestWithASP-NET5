4399eafa8cf5f41ae89facada4b3b9cf79a86f61
Marcus Vinícius <mviniciuscastro@gmail.com>
2020-10-21 19:32:38 -0300

    PessoaControler

---
+++ Calculadora/ApiREST/Controllers/PessoaController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiREST.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        
+
+        private readonly ILogger<PessoaController> _logger;
+
+        public PessoaController(ILogger<PessoaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("acao/{acao}/{primeiro}/{segundo}")]
+        public IActionResult Get(string acao, string primeiro, string segundo)
+        {
+            if (!isNumeric(primeiro) || !isNumeric(segundo))
+            {
+                return BadRequest("Valores inválidos");
+            }
+
+            decimal valor = 0;
+            switch (acao)
+            {
+                case "soma":
+                    valor = soma(primeiro, segundo);
+                    break;
+                case "subtrair":
+                    valor = subtrair(primeiro, segundo);
+                    break;
+                case "divisao":
+                    valor = divisao(primeiro, segundo);
+                    break;
+                case "multiplicacao":
+                    valor = multiplicacao(primeiro, segundo);
+                    break;
+
+            }
+
+            return Ok(valor);
+        }
+
+
+    }
+}
---
+++ Calculadora/ApiREST/Model/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiREST.Model
+{
+    public class Pessoa
+    {
+        public long Id { get; set; }
+        public long PrimeiroNome { get; set; }
+        public long SobreNome { get; set; }
+        public long Endereco { get; set; }
+        public long Sexo { get; set; }
+
+    }
+}
